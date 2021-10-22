ea45c9b81a37501c44038c672e0ca5f04c4c7e35
sadulla <ubaydullayevsadulla843@gmail.com>
2021-10-22 14:18:38 +0500

    finished app

--- app/src/main/java/com/intentsoft/qrcodescanner/ui/fragment/ScannerHistoryFragment.kt
+++ app/src/main/java/com/intentsoft/qrcodescanner/ui/fragment/ScannerHistoryFragment.kt
@@ -1,20 +1,31 @@
 package com.intentsoft.qrcodescanner.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.intentsoft.qrcodescanner.R
+import com.intentsoft.qrcodescanner.db.DbHelper
+import com.intentsoft.qrcodescanner.db.DbHelperI
+import com.intentsoft.qrcodescanner.db.database.QrResultDataBase
+import com.intentsoft.qrcodescanner.db.entitles.QrResult
+import com.intentsoft.qrcodescanner.ui.adapter.ScannedResultListAdapter
+import com.intentsoft.qrcodescanner.utils.gone
+import com.intentsoft.qrcodescanner.utils.visible
+import kotlinx.android.synthetic.main.fragment_scanner_history.view.*
+import kotlinx.android.synthetic.main.layout_header_history.view.*
 import java.io.Serializable
 
 
 class ScannerHistoryFragment : Fragment() {
-    private lateinit var resultType: ResultListType
-    private lateinit var mView: View
+
     enum class ResultListType : Serializable {
         ALL_RESULT, FAVOURITE_RESULT
     }
+
     companion object {
 
         private const val ARGUMENT_RESULT_LIST_TYPE = "ArgumentResultType"
@@ -28,19 +39,115 @@ class ScannerHistoryFragment : Fragment() {
         }
     }
 
+
+    private lateinit var mView: View
+
+    private lateinit var dbHelperI: DbHelperI
+
+    private var resultListType: ResultListType? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        handleArgument()
+        handleArguments()
     }
 
-    private fun handleArgument() {
-        resultType = arguments?.getSerializable(ARGUMENT_RESULT_LIST_TYPE) as ResultListType
+    private fun handleArguments() {
+        resultListType = arguments?.getSerializable(ARGUMENT_RESULT_LIST_TYPE) as ResultListType
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        mView =  inflater.inflate(R.layout.fragment_scanner_history, container, false)
-        return mView
+        mView = inflater.inflate(R.layout.fragment_scanner_history, container, false)
+        init()
+        setSwipeRefresh()
+        onClicks()
+        showListOfResults()
+        return mView.rootView
+    }
+
+    private fun init() {
+        dbHelperI = DbHelper(QrResultDataBase.getAppDatabase(context!!)!!)
+        mView.layoutHeader.tvHeaderText.text = getString(R.string.recent_scanned_results)
+    }
+
+    private fun showListOfResults() {
+        when (resultListType) {
+            ResultListType.ALL_RESULT -> showAllResults()
+            ResultListType.FAVOURITE_RESULT -> showFavouriteResults()
+        }
     }
 
 
+    private fun showAllResults() {
+        val listOfAllResult = dbHelperI.getAllQRScannedResult()
+        showResults(listOfAllResult)
+        mView.layoutHeader.tvHeaderText.text = getString(R.string.recent_scanned)
+    }
+
+    private fun showFavouriteResults() {
+        val listOfFavouriteResult = dbHelperI.getAllFavouriteQRScannedResult()
+        showResults(listOfFavouriteResult)
+        mView.layoutHeader.tvHeaderText.text = getString(R.string.favourites_scanned_results)
+    }
+
+
+    private fun showResults(listOfQrResult: List<QrResult>) {
+        if (listOfQrResult.isNotEmpty())
+            initRecyclerView(listOfQrResult)
+        else
+            showEmptyState()
+    }
+
+    private fun initRecyclerView(listOfQrResult: List<QrResult>) {
+        mView.scannedHistoryRecyclerView.layoutManager = LinearLayoutManager(context)
+        mView.scannedHistoryRecyclerView.adapter =
+            ScannedResultListAdapter(dbHelperI, context!!, listOfQrResult.toMutableList())
+        showRecyclerView()
+    }
+
+    private fun setSwipeRefresh() {
+        mView.swipeRefresh.setOnRefreshListener {
+            mView.swipeRefresh.isRefreshing = false
+            showListOfResults()
+        }
+    }
+
+
+    private fun onClicks() {
+        mView.layoutHeader.removeAll.setOnClickListener {
+            showRemoveAllScannedResultDialog()
+        }
+    }
+
+    private fun showRemoveAllScannedResultDialog() {
+        AlertDialog.Builder(context!!, R.style.CustomAlertDialog).setTitle(getString(R.string.clear_all))
+            .setMessage(getString(R.string.clear_all_result))
+            .setPositiveButton(getString(R.string.clear)) { _, _ ->
+                clearAllRecords()
+            }
+            .setNegativeButton(getString(R.string.cancel)) { dialog, _ ->
+                dialog.cancel()
+            }.show()
+
+    }
+
+    private fun clearAllRecords() {
+        when (resultListType) {
+            ResultListType.ALL_RESULT -> dbHelperI.deleteAllQRScannedResult()
+            ResultListType.FAVOURITE_RESULT -> dbHelperI.deleteAllFavouriteQRScannedResult()
+        }
+        mView.scannedHistoryRecyclerView.adapter?.notifyDataSetChanged()
+        showListOfResults()
+    }
+
+    private fun showRecyclerView() {
+        mView.layoutHeader.removeAll.visible()
+        mView.scannedHistoryRecyclerView.visible()
+        mView.noResultFound.gone()
+    }
+
+    private fun showEmptyState() {
+        mView.layoutHeader.removeAll.gone()
+        mView.scannedHistoryRecyclerView.gone()
+        mView.noResultFound.visible()
+    }
 }
--- app/src/main/java/com/intentsoft/qrcodescanner/ui/main/MainPagerAdapter.kt
+++ app/src/main/java/com/intentsoft/qrcodescanner/ui/main/MainPagerAdapter.kt
@@ -6,7 +6,7 @@ import androidx.fragment.app.FragmentStatePagerAdapter
 import com.intentsoft.qrcodescanner.ui.fragment.QRScannerFragment
 import com.intentsoft.qrcodescanner.ui.fragment.ScannerHistoryFragment
 
-class MainPagerAdapter(var fm: FragmentManager): FragmentStatePagerAdapter(fm) {
+class MainPagerAdapter(fm: FragmentManager): FragmentStatePagerAdapter(fm) {
     override fun getCount(): Int {
         return 3
     }
@@ -14,8 +14,8 @@ class MainPagerAdapter(var fm: FragmentManager): FragmentStatePagerAdapter(fm) {
     override fun getItem(position: Int): Fragment {
         return when(position){
             0 -> QRScannerFragment.newInstance()
-            1 -> ScannerHistoryFragment.newInstance()
-            2 -> ScannerHistoryFragment.newInstance()
+            1 -> ScannerHistoryFragment.newInstance(ScannerHistoryFragment.ResultListType.ALL_RESULT)
+            2 -> ScannerHistoryFragment.newInstance(ScannerHistoryFragment.ResultListType.FAVOURITE_RESULT)
             else -> QRScannerFragment.newInstance()
         }
     }
